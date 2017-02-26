0d08f0e0f435a94550dd5a25829a87871a0323d2
Nitesh Arora <nitesharora21@gmail.com>
2017-02-26 02:16:40 -0500

    Added Navigation Drawer to the Main Activity (First Draft)
    TODO:
    1. Reformat the Navigation Drawer menu items
    2. Close the Navigation Drawer menu when the Main Activity is closed

--- app/src/main/java/com/capstone490/nitesh/datadashboard/MainActivity.java
+++ app/src/main/java/com/capstone490/nitesh/datadashboard/MainActivity.java
@@ -3,6 +3,7 @@ package com.capstone490.nitesh.datadashboard;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(MainActivity.this, Navigation_Drawer.class);
+        startActivity(intent);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
